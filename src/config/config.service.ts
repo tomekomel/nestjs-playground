73267fee935632afdb8709386aa0bc4e1f4ad23b
Tomasz Omeljaniuk <tomekomel@gmail.com>
2023-03-31 16:02:00 +0200

    feat: typescript playground

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -9,8 +9,21 @@ export class ConfigService {
   private readonly envConfig: EnvConfig;
 
   constructor(@Inject('CONFIG_OPTIONS') private options: Record<string, any>) {
+    /*** TypeScript Playground ***/
+    const configProps = { folder: 'test' };
+    configProps.folder = 'test7';
+
+    const req = { url: "https://example.com", method: "GET" } as const;
+    // req.url = 'www';
+    /*** End of TypeScript Playground ***/
+
     const filePath = `${process.env.NODE_ENV || 'development'}.env`;
-    const envFile = path.resolve(__dirname, '../../src/', options.folder, filePath);
+    const envFile = path.resolve(
+      __dirname,
+      '../../src/',
+      options.folder,
+      filePath,
+    );
     this.envConfig = dotenv.parse(fs.readFileSync(envFile));
   }
 
